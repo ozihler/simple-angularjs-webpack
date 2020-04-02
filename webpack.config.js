8f588bf02a75a47cf487a37094ad197dd159227e
ozihler <ozihler@gmail.com>
2020-04-02 10:01:05 +0200

    simplify for display in word

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const path = require("path");
 
 module.exports = {
     entry: './app/app.js',
-    output: {
-        filename: 'my-first-webpack.bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(html)$/,
-                use: 'html-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-                template: './app/index.html'
-            }
-        )
-    ]
+    output: {filename: 'my-first-webpack.bundle.js', path: path.resolve(__dirname, 'dist')},
+    module: {rules: [{test: /\.(html)$/, use: 'html-loader'}]},
+    plugins: [new HtmlWebpackPlugin({template: './app/index.html'})]
 };
